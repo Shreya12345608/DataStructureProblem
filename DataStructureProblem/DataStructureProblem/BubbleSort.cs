e10ab8584abdb8523726d13622ecb8e18914377b
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-17 21:12:36 +0530

    [Shreya] Add: Program for Bubble Sort

---
+++ DataStructureProblem/DataStructureProblem/BubbleSort.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblem
+{
+    class BubbleSort
+    {
+
+        public int[] arr;
+        public int Top;
+        public int numElements;
+
+        /// <summary>
+        /// constructor for bubble sort with parametr size
+        /// </summary>
+        /// <param name="size"></param>
+        public BubbleSort(int size)
+        {
+            // storing int size in arr
+            arr = new int[size];
+            //top = size - 1
+            Top = size - 1;
+            // by default numElement is 0
+            numElements = 0;
+        }
+        /// <summary>
+        /// insert array 
+        /// </summary>
+        /// <param name="item"></param>
+        public void InsertNumbers(int item)
+        {
+
+            arr[numElements] = item;
+            numElements++;
+        }
+        /* Prints the array */
+        public void DisplayElements()
+        {
+            //loop if display = 0 display <= top value ; den increment by 1
+            for (int display = 0; display <= Top; display++)
+                // display array list ,
+                Console.WriteLine(arr[display] + ",");
+        }
+
+        /// <summary>
+        ///method for  bubble sort 
+        /// </summary>
+        public void BubbleSorts()
+        {
+            // instance variable
+            int temp;
+            //loop taking top value in outer ; outer > =1 ; outer +1
+            for (int outer = Top; outer >= 1; outer--)
+            {
+                //loop for inner is 0 ; inner < =1 ; inner +1
+                for (int inner = 0; inner <= outer - 1; inner++)
+                    if ((int)arr[inner] > arr[inner + 1])
+                    {
+                        // swap temp and arr[inner]
+                        temp = arr[inner];
+                        arr[inner] = arr[inner + 1];
+                        arr[inner + 1] = temp;
+                    }
+            }
+            //last display Elements
+            this.DisplayElements();
+        }
+    }
+}
---
+++ DataStructureProblem/DataStructureProblem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Problems");
+            Console.WriteLine("Press 1 for Bubble Sort Problem");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    var Output = new BubbleSort(10);
+                    Random random = new Random(20);
+                    for (int i = 0; i < 10; i++)
+                     Output.InsertNumbers((int)(random.NextDouble() * 10));
+                    Console.WriteLine("Before sorting:");
+                    Output.DisplayElements();
+                    Console.WriteLine("During sorting:");
+                    Output.BubbleSorts();
+                    Console.WriteLine("After sorting:");
+                    Output.DisplayElements();
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection. Enter a valid option:");
+                    break;
+            }
+        }
+    }
+}
+
